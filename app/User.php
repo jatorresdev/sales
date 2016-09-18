d7536b56cb752a7e1182ea27353570dc05b3b49d
Angel Torres <jatorresdev@gmail.com>
2016-09-17 21:14:56 -0500

    Migración usuarios

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'name', 'last_name', 'email', 'cellphone', 'telephone'
     ];
 
     /**
